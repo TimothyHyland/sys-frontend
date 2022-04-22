35f404c0f2a1a58f69f143d3826dcc4bd60d7491
Tim Hyland <moldymilk@hotmail.com>
2022-04-22 16:01:55 -0400

    Update index.ts

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -36,8 +36,7 @@ export const walletconnect = new WalletConnectConnector({
   rpc: { 57: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
-  pollingInterval: 15000
-})
+ })
 
 // mainnet only
 export const fortmatic = new FortmaticConnector({
